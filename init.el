51e798e1e7b710bd61508528ef37a80047676e41
James <james.asghar@gmail.com>
2015-01-01 20:21:55 -0600

    Added package-archive and coffee-mode

--- init.el
+++ init.el
@@ -1,12 +1,27 @@
+ ;; package initializer
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+ ;; smited the tool-bar
 (tool-bar-mode -1)
+
+ ;; loads custom lush theme
 (load-theme 'lush t)
+
+ ;; gets rid of ugly scroll-bar
 (scroll-bar-mode -1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
